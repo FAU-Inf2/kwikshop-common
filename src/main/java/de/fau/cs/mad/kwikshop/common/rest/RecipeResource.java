88ef9738aa49bc380e318d66ad9a6ceb44c8a4c8
Andreas Grünwald <andreasgruenwald@live.de>
2015-07-21 17:11:56 +0200

    Removed ListResource, added methods directly to ShoppingListResource/RecipeResouce because code generation does not seem to handle interface inheritance very well

--- src/main/java/de/fau/cs/mad/kwikshop/common/rest/RecipeResource.java
+++ src/main/java/de/fau/cs/mad/kwikshop/common/rest/RecipeResource.java
@@ -2,11 +2,13 @@ package de.fau.cs.mad.kwikshop.common.rest;
 
 
 import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 
+import de.fau.cs.mad.kwikshop.common.Item;
 import de.fau.cs.mad.kwikshop.common.RecipeServer;
 import de.fau.cs.mad.kwikshop.common.User;
 
-import de.fau.cs.mad.kwikshop.common.rest.ListResource;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
 
@@ -17,7 +19,7 @@ import java.util.List;
 
 @Path("recipe")
 @Api(value = "recipe", description = "Storage and retrieval of recipes")
-public interface RecipeResource extends ListResource {
+public interface RecipeResource {
 
 
 
@@ -45,5 +47,56 @@ public interface RecipeResource extends ListResource {
     @Produces(MediaType.APPLICATION_JSON)
     RecipeServer updateList(@Auth User user, @PathParam("listId") int listId, RecipeServer list, @QueryParam("updateItems") boolean updateItems);
 
+    @DELETE
+    @UnitOfWork
+    @Path("{listId}")
+    void deleteList(@Auth User user, @PathParam("listId") int listId);
 
+    @GET
+    @UnitOfWork
+    @Path("{listId}/{itemId}")
+    Item getListItem(@Auth User user, @PathParam("listId") int listId, @PathParam("itemId") int itemId);
+
+
+    @PUT
+    @UnitOfWork
+    @Path("{listId}/newItem")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Create a new Item",
+            notes = "Inserts a new Item into the database. Returns the new Item object including id.",
+            response = Item.class)
+    Item createItem(@Auth User user,
+                    @PathParam("listId") int listId,
+                    @ApiParam(value = "Item to create", required = true)Item newItem);
+
+    @POST
+    @UnitOfWork
+    @Path("{listId}/{itemId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Update an existing item",
+            notes = "Updates the details of an existing item. Returns the updated Item object.",
+            response = Item.class)
+    Item updateItem(
+            @Auth User user,
+            @PathParam("listId") @ApiParam(value ="id of the list the item belongs to", required = true) int listId,
+            @PathParam("itemId") @ApiParam(value = "id of the Item to update", required = true)  int itemId,
+            @ApiParam(value = "new details of the specified item", required = true) Item item);
+
+    @DELETE
+    @UnitOfWork
+    @Path("{listId}/{itemId}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Delete an Item",
+            notes = "Removes a Item from the database.",
+            response = Item.class)
+    void deleteListItem(
+            @Auth User user,
+            @PathParam("listId") @ApiParam(value ="id of the list the item belongs to", required = true)  int listId,
+            @PathParam("itemId") @ApiParam(value = "id of the Item to update", required = true) int itemId);
 }
--- src/main/java/de/fau/cs/mad/kwikshop/common/rest/ShoppingListResource.java
+++ src/main/java/de/fau/cs/mad/kwikshop/common/rest/ShoppingListResource.java
@@ -2,6 +2,10 @@ package de.fau.cs.mad.kwikshop.common.rest;
 
 
 import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+import de.fau.cs.mad.kwikshop.common.Item;
 import de.fau.cs.mad.kwikshop.common.ShoppingListServer;
 import de.fau.cs.mad.kwikshop.common.User;
 import io.dropwizard.auth.Auth;
@@ -14,7 +18,7 @@ import java.util.List;
 
 @Path("shoppinglist")
 @Api(value = "shoppinglist", description = "Storage and retrieval of shopping lists")
-public interface ShoppingListResource extends ListResource {
+public interface ShoppingListResource {
 
 
 
@@ -42,5 +46,57 @@ public interface ShoppingListResource extends ListResource {
     @Produces(MediaType.APPLICATION_JSON)
     ShoppingListServer updateList(@Auth User user, @PathParam("listId") int listId, ShoppingListServer list, @QueryParam("updateItems") boolean updateItems);
 
+    @DELETE
+    @UnitOfWork
+    @Path("{listId}")
+    void deleteList(@Auth User user, @PathParam("listId") int listId);
+
+    @GET
+    @UnitOfWork
+    @Path("{listId}/{itemId}")
+    Item getListItem(@Auth User user, @PathParam("listId") int listId, @PathParam("itemId") int itemId);
+
+
+    @PUT
+    @UnitOfWork
+    @Path("{listId}/newItem")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Create a new Item",
+            notes = "Inserts a new Item into the database. Returns the new Item object including id.",
+            response = Item.class)
+    Item createItem(@Auth User user,
+                    @PathParam("listId") int listId,
+                    @ApiParam(value = "Item to create", required = true)Item newItem);
+
+    @POST
+    @UnitOfWork
+    @Path("{listId}/{itemId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Update an existing item",
+            notes = "Updates the details of an existing item. Returns the updated Item object.",
+            response = Item.class)
+    Item updateItem(
+            @Auth User user,
+            @ApiParam(value ="id of the list the item belongs to", required = true) @PathParam("listId") int listId,
+            @ApiParam(value = "id of the Item to update", required = true) @PathParam("itemId") int itemId,
+            @ApiParam(value = "new details of the specified item", required = true) Item item);
+
+    @DELETE
+    @UnitOfWork
+    @Path("{listId}/{itemId}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Delete an Item",
+            notes = "Removes a Item from the database.",
+            response = Item.class)
+    void deleteListItem(
+            @Auth User user,
+            @ApiParam(value ="id of the list the item belongs to", required = true) @PathParam("listId") int listId,
+            @ApiParam(value = "id of the Item to update", required = true) @PathParam("itemId") int itemId);
 
 }
